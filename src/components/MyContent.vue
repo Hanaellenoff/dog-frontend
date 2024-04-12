259ab75681723fa145f82fdb07536cc4707cb613
rubygomez <rvbii.gomez@gmail.com>
2024-04-12 11:01:06 -0700

    some in content

--- src/components/MyContent.vue
+++ src/components/MyContent.vue
@@ -1,7 +1,14 @@
-<script></script>
+<script>
+import MyModal from './MyModal.vue';
+
+// add "Modal," in export default
+</script>
 <template>
     <main>
-        <h1>Hi</h1>
+        <MyModal v-bind:show="true">
+            <h1>Test</h1>
+        </MyModal>
     </main>
 </template>
+
 <style></style>
