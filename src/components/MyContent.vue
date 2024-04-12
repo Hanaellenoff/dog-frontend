526e4b83b484062cc1f8d03564e52d439c41f7f7
Hanaellenoff <hanaellenoff@gmail.com>
2024-04-12 14:02:20 -0400

    added index

--- src/components/MyContent.vue
+++ src/components/MyContent.vue
@@ -1,7 +1,24 @@
-<script></script>
+<script>
+import DogsIndex from "./DogsIndex.vue";
+export default {
+  components: {
+    DogsIndex,
+  },
+  data: function () {
+    return {
+      dogs: [
+        { id: 1, name: "Teddy", age: 3, breed: "Golden retriever" },
+        { id: 2, name: "Chloe", age: 6, breed: "Collie" },
+      ],
+    };
+  },
+};
+</script>
+
 <template>
-    <main>
-        <h1>Hi</h1>
-    </main>
+  <main>
+    <DogsIndex v-bind:dogs="dogs" />
+  </main>
 </template>
+
 <style></style>
