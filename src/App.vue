694a954d89129ca750d06cfb383b6801f2404171
rubygomez <rvbii.gomez@gmail.com>
2024-04-12 10:31:40 -0700

    MyContent/Footer/HEader components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
+<script>
+import MyHeader from "./components/MyHeader.vue";
+import MyContent from "./components/MyContent.vue";
+import MyFooter from "./components/MyFooter.vue";
+
+export default {
+  name: "app",
+  components: {
+    MyHeader,
+    MyContent,
+    MyFooter,
+  },
+};
+</script>
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MyHeader />
+    <MyContent />
+    <MyFooter />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
